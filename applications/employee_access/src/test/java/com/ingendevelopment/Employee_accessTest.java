8b3fb779f90df0f52240ccc74168d398ced4d975
Brian Smith <crazycabo350@gmail.com>
2025-01-28 22:01:05 -0500

    Create consumer service API (#19)

---
+++ applications/employee_access/src/test/java/com/ingendevelopment/Employee_accessTest.java
@@ -0,0 +1,21 @@
+package com.ingendevelopment;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class Employee_accessTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
