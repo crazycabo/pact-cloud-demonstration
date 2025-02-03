af82d57ab9858f9ae9eebaf824d9dc9ebb699ff7
Brian Smith <crazycabo350@gmail.com>
2025-02-03 16:21:02 -0500

    add initial consumer service and client

---
+++ applications/employee_access/src/main/java/com/ingendevelopment/EmployeeDirectoryClient.java
@@ -0,0 +1,13 @@
+package com.ingendevelopment;
+
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.client.annotation.Client;
+
+import java.util.Optional;
+
+@Client("http://example.com/employee-directory")
+public interface EmployeeDirectoryClient {
+
+    @Get("/{?employeeId}")
+    Optional<EmployeeInfo> getEmployee(String employeeId);
+}
