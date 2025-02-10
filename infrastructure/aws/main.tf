7d7a6d687cfb1420327bebced3c60aa61cfb480e
Brian Smith <crazycabo350@gmail.com>
2025-02-10 02:24:38 -0500

    update ECS task execution role

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -110,6 +110,7 @@ resource "aws_ecs_task_definition" "pactbroker_app_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.packbroker_ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -135,8 +136,8 @@ resource "aws_ecs_task_definition" "pactbroker_app_task" {
   ])
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+resource "aws_iam_role" "packbroker_ecs_task_execution_role" {
+  name = "packbroker_ecs_task_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -153,10 +154,9 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_logs" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+  role       = aws_iam_role.packbroker_ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-
 
 resource "aws_ecs_service" "pactbroker_app_service" {
   name            = "pactbroker-app-service"
