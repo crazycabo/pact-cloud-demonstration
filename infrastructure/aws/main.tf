8f973ba8ab95f05b917d382cce4f13b7921db669
Brian Smith <crazycabo350@gmail.com>
2025-02-10 15:37:11 -0500

    permit open egress access for ECS

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -234,10 +234,10 @@ resource "aws_security_group" "ecs_sg" {
   vpc_id      = module.vpc.vpc_id
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    security_groups = [aws_security_group.rds_sg.id]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
