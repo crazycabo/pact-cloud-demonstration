84b3ffafd4386c84335abf9468ac1f1d7b99ba9c
Brian Smith <crazycabo350@gmail.com>
2025-02-10 01:53:20 -0500

    include ECS logging config

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -104,8 +104,10 @@ resource "aws_ecs_cluster" "pactbroker_app_cluster" {
 
   configuration {
     execute_command_configuration {
+      logging = "OVERRIDE"
+
       log_configuration {
-        cloud_watch_encryption_enabled = true
+        cloud_watch_encryption_enabled = false
         cloud_watch_log_group_name     = aws_cloudwatch_log_group.pactbroker.name
       }
     }
