087b5a42be97a5c69bb417b265903c43059976f1
Brian Smith <crazycabo350@gmail.com>
2025-02-10 15:18:12 -0500

    create ECS auto-scaling config

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -284,6 +284,32 @@ resource "aws_ecs_service" "pactbroker_app_service" {
   }
 }
 
+resource "aws_appautoscaling_target" "pact_broker" {
+  max_capacity       = 2
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.pactbroker_app_cluster.name}/${aws_ecs_service.pactbroker_app_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "pact_broker_cpu" {
+  name               = "pact-broker-cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.pact_broker.resource_id
+  scalable_dimension = aws_appautoscaling_target.pact_broker.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.pact_broker.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 60
+    scale_out_cooldown = 60
+    scale_in_cooldown  = 300
+  }
+}
+
 # ----------
 # RDS
 # ----------
