bc150d77d0d9920da58151b426e33d12623316b4
Brian Smith <crazycabo350@gmail.com>
2025-02-10 11:49:41 -0500

    permit all ingress to RDS

--- infrastructure/aws/main.tf
+++ infrastructure/aws/main.tf
@@ -267,7 +267,7 @@ resource "aws_security_group" "rds_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    security_groups = [aws_security_group.pact_broker_alb_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
